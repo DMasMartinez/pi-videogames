548a18e1c165c83f74f6f26c1a419595250615ce
Daniel <danielmasmar@gmail.com>
2024-05-14 17:52:22 -0400

    haciendo coneccion externa con render

--- Proyecto/Frontend/Backend/src/db.js
+++ Proyecto/Frontend/Backend/src/db.js
@@ -1,25 +1,25 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 // hoooolllllaaaaa
-const { USER, PASSWORD, HOST, PORT, BDD } = process.env;
+const { USER, PASSWORD, HOST, PORT, BDD, DB_DEPLOY } = process.env;
 const Gamefunction = require("./models/game");
 const Genrefunction = require("./models/genre");
 
-const database = new Sequelize(
-  `postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${BDD}`,
-  { logging: false }
-);
-// const database = new Sequelize(DB_DEPLOY1, {
-//   dialect: "postgres",
-//   logging: false,
-//   native: false,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false, // Usar false si no tienes un certificado de CA válido
-//     },
-//   },
-// });
+// const database = new Sequelize(
+//   `postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${BDD}`,
+//   { logging: false }
+// );
+const database = new Sequelize(DB_DEPLOY, {
+  dialect: "postgres",
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Usar false si no tienes un certificado de CA válido
+    },
+  },
+});
 Gamefunction(database);
 Genrefunction(database);
 
