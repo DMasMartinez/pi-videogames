0dc214f9fc2467e4cd0475562c65fd779c5fd89d
Daniel <danielmasmar@gmail.com>
2024-05-13 21:20:46 -0400

    nueva rama

--- Proyecto/Frontend/Backend/src/router/gamehandler/gamehandler.js
+++ Proyecto/Frontend/Backend/src/router/gamehandler/gamehandler.js
@@ -1,11 +1,11 @@
+const { Router } = require("express");
+const gamehandler = Router();
 
-const { Router } = require('express')
-const gamehandler = Router()
-
-const findallgames = require('./controllers/findallgames')
-const findgamebyname = require('./controllers/findgamebyname')
-const findgamebyid = require('./controllers/findgamebyid')
-const creategame = require('./controllers/creategame')
+const findallgames = require("./controllers/findallgames");
+const findgamebyname = require("./controllers/findgamebyname");
+const findgamebyid = require("./controllers/findgamebyid");
+const creategame = require("./controllers/creategame");
+const uploadGamePicture = require("../middleware/uploadGamePicture");
 
 // gamehandler.get('/',async(req,res)=>{
 //     try{
@@ -16,36 +16,58 @@ const creategame = require('./controllers/creategame')
 //     }
 // })
 
-gamehandler.get('/',async(req,res)=>{
-    
-    try{
-        const {name} = req.query
-        const gamebyname = name?await findgamebyname(name):await findallgames()
-        res.status(200).json(gamebyname)
-    }catch(error){
-        res.status(500).json({error:error.message})
-    }
-})
+gamehandler.get("/", async (req, res) => {
+  try {
+    const { name } = req.query;
+    const gamebyname = name ? await findgamebyname(name) : await findallgames();
+    res.status(200).json(gamebyname);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-gamehandler.get('/:id',async(req,res)=>{
-    
-    try{
-        const {id} = req.params
-        const gamebyid = await findgamebyid(id)
-        res.status(200).json(gamebyid)
-    }catch(error){
-        res.status(500).json({error:error.message})
+gamehandler.post(
+  "/upload-profile-picture",
+  uploadGamePicture,
+  async (req, res) => {
+    try {
+      console.log(req.file.path);
+      const imageUrl = req.file.path;
+      res.status(200).json({ imageUrl: imageUrl });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ error: error.message });
     }
-})
+  }
+);
 
-gamehandler.post('/',async(req,res)=>{
-    
-    try{
-        const {id,name,Genres,description,release,image,devices,ratings} = req.body
-        const newgame = await creategame({id,name,Genres,description,release,image,devices,ratings})
-        res.status(200).json(newgame)
-    }catch(error){
-        res.status(500).json({error:error.message})
-    }
-})
-module.exports = gamehandler
+gamehandler.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const gamebyid = await findgamebyid(id);
+    res.status(200).json(gamebyid);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+gamehandler.post("/", async (req, res) => {
+  try {
+    const { id, name, Genres, description, release, image, devices, ratings } =
+      req.body;
+    const newgame = await creategame({
+      id,
+      name,
+      Genres,
+      description,
+      release,
+      image,
+      devices,
+      ratings,
+    });
+    res.status(200).json(newgame);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+module.exports = gamehandler;
